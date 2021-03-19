2ad6c024a8867194e8cbd30e2aa11df8601c2838
jrdannug <johndannug17@gmail.com>
2021-03-19 22:56:55 +0800

    implemented custom scope for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'angular-items', to: 'portfolios#angular'
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
